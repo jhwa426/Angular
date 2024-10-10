e5dd361e495279fd31ba0d2f8c114f41b43aa801
Jeff Hwang <wodud6359@gmail.com>
2024-10-11 09:37:42 +1300

    components - header

---
+++ Webshop/client/src/app/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    imports: [MatToolbarModule, MatIconModule, MatBadgeModule, MatMenuModule],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
+})
+
+
+export class HeaderComponent {
+
+
+
+
+}
