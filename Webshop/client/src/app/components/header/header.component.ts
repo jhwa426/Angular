cc1132f0f701c3526947b7eb5073aa782f865d1a
Jeff Hwang <wodud6359@gmail.com>
2024-10-14 14:26:04 +1300

    cart

--- Webshop/client/src/app/components/header/header.component.ts
+++ Webshop/client/src/app/components/header/header.component.ts
@@ -3,11 +3,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatMenuModule } from '@angular/material/menu';
+import { CurrencyPipe } from '@angular/common'
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [MatToolbarModule, MatIconModule, MatBadgeModule, MatMenuModule],
+    imports: [MatToolbarModule, MatIconModule, MatBadgeModule, MatMenuModule, CurrencyPipe],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
@@ -15,7 +16,11 @@ import { MatMenuModule } from '@angular/material/menu';
 
 export class HeaderComponent {
 
+    getTotal(items: number) {
 
+    }
 
+    onClearCart(): void {
 
+    }
 }
