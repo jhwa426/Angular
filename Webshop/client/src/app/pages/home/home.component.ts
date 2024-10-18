41be4e733b7ad7c39c5c5e21ce3acef39a7ed002
Jeff Hwang <wodud6359@gmail.com>
2024-10-18 13:49:18 +1300

    filters

--- Webshop/client/src/app/pages/home/home.component.ts
+++ Webshop/client/src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ProductsHeaderComponent } from "./components/products-header/products-header/products-header.component";
+import { FiltersComponent } from "./components/filters/filters.component";
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [MatSidenavModule, ProductsHeaderComponent],
+    imports: [MatSidenavModule, ProductsHeaderComponent, FiltersComponent, MatGridListModule, CommonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
@@ -14,9 +17,15 @@ import { ProductsHeaderComponent } from "./components/products-header/products-h
 
 export class HomeComponent {
     cols = 3;
+    category: string | undefined;
+
 
     onColumnsCountChange(colsNum: number): void {
         this.cols = colsNum;
+    }
+
+    onShowCategory(newCategory: string): void {
+        this.category = newCategory;
     }
 
 }
