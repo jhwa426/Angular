49ecfa99c1c6e5a5a8f9e4d843b08ca2a65538cb
Jeff Hwang <jeff@phntms.com>
2025-01-14 10:49:46 +1300

    Refactor product component

--- Projects/original-clothing/client/src/app/components/product/product.component.ts
+++ Projects/original-clothing/client/src/app/components/product/product.component.ts
@@ -1,53 +1,60 @@
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { Product } from "../../../type";
-import { RatingModule } from 'primeng/rating';
-import { FormsModule } from "@angular/forms";
-import { ButtonModule } from "primeng/button";
-import { ConfirmationService } from "primeng/api";
-import { PricePipe } from "../../pipes/price.pipe";
-import { TruncateNamePipe } from "../../pipes/truncate-name.pipe";
+import {Component, input, output, viewChild} from '@angular/core';
+import {Product} from '../../../type';
+import {RatingModule} from 'primeng/rating';
+import {FormsModule} from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+import {ConfirmationService} from 'primeng/api';
+import {PricePipe} from '../../pipes/price.pipe';
+import {TruncateNamePipe} from '../../pipes/truncate-name.pipe';
 
 @Component({
-    selector: 'app-product',
-    standalone: true,
-    imports: [RatingModule, FormsModule, ButtonModule, PricePipe, TruncateNamePipe],
-    providers: [ConfirmationService],
-    templateUrl: './product.component.html',
-    styleUrl: './product.component.css'
+  selector: 'app-product',
+  standalone: true,
+  imports: [
+    RatingModule,
+    FormsModule,
+    ButtonModule,
+    PricePipe,
+    TruncateNamePipe,
+  ],
+  providers: [ConfirmationService],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss',
 })
-
-
 export class ProductComponent {
-    constructor(private confirmationService: ConfirmationService) { }
+  private readonly deleteButton = viewChild<any>('deleteButton');
 
-    @ViewChild('deleteButton') deleteButton: any;
+  productOutput = output<Product>;
 
-    @Input() product!: Product;
-    @Output() productOutput: EventEmitter<Product> = new EventEmitter<Product>();
+  edit = output<Product>();
+  delete = output<Product>();
+  product = input<Product>();
 
-    @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
-    @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
+  constructor(private confirmationService: ConfirmationService) {}
 
-    ngOnInit() {
+  ngOnInit() {}
 
+  editProduct() {
+    const currentProduct = this.product();
+    if (currentProduct) {
+      this.edit.emit(currentProduct);
     }
+  }
 
-    editProduct() {
-        this.edit.emit(this.product);
+  confirmDelete() {
+    this.confirmationService.confirm({
+      target: this.deleteButton().nativeElement,
+      message: 'Are you sure that you want to delete this product?',
+      accept: () => {
+        this.deleteProduct();
+      },
+    });
+  }
+
+  deleteProduct() {
+    const currentProduct = this.product();
+    if (currentProduct) {
+      this.delete.emit(currentProduct);
     }
-
-    confirmDelete() {
-        this.confirmationService.confirm({
-            target: this.deleteButton.nativeElement,
-            message: 'Are you sure that you want to delete this product?',
-            accept: () => {
-                this.deleteProduct();
-            },
-        });
-    }
-
-    deleteProduct() {
-        this.delete.emit(this.product);
-    }
-
+  }
 }
