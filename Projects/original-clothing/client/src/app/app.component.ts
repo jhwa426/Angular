f2ae8e766f3a6f044d4a099fa5560b404b945dc9
Jeff Hwang <jeff@phntms.com>
2025-01-14 08:45:01 +1300

    Migration to project

--- Projects/original-clothing/client/src/app/app.component.ts
+++ Projects/original-clothing/client/src/app/app.component.ts
@@ -1,16 +1,13 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {HomeComponent} from './home/home.component';
 import {HeaderComponent} from './layout/header/header.component';
 import {FooterComponent} from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Angular Tutorial';
-}
+export class AppComponent {}
