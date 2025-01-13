e86ca9b0d3ebf8dd23f690b7339cefcc3c5103b7
Jeff Hwang <jeff@phntms.com>
2025-01-13 17:01:40 +1300

    Fix eslint and delete test files

--- Tutorials/ng-tutorial-2/client/src/app/app.component.ts
+++ Tutorials/ng-tutorial-2/client/src/app/app.component.ts
@@ -1,18 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
-import { HeaderComponent } from "./layout/header/header.component";
-import { FooterComponent } from "./layout/footer/footer.component";
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {FooterComponent} from './layout/footer/footer.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
-    templateUrl: "./app.component.html",
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
-
-
 export class AppComponent {
-    title = 'Angular Tutorial';
+  title = 'Angular Tutorial';
 }
--- Tutorials/ng-tutorial-2/client/src/app/app.config.ts
+++ Tutorials/ng-tutorial-2/client/src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 
-import { provideHttpClient } from "@angular/common/http";
+import {provideHttpClient} from '@angular/common/http';
 
-import { provideAnimations } from "@angular/platform-browser/animations";
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideHttpClient(), provideAnimations()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimations()],
 };
