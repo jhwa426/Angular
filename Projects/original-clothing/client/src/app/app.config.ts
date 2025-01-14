3ab1e65737bc340f0d1627c52e41247731d43301
Jeff Hwang <jeff@phntms.com>
2025-01-14 15:12:05 +1300

    Fix eslint

--- Projects/original-clothing/client/src/app/app.config.ts
+++ Projects/original-clothing/client/src/app/app.config.ts
@@ -1,10 +1,7 @@
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
-
 import {routes} from './app.routes';
-
 import {provideHttpClient} from '@angular/common/http';
-
 import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
--- Projects/original-clothing/client/src/app/app.routes.ts
+++ Projects/original-clothing/client/src/app/app.routes.ts
@@ -3,17 +3,6 @@ import {HomeComponent} from './home/home.component';
 import {AboutUsComponent} from './about-us/about-us.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'about-us',
-    component: AboutUsComponent,
-  },
-  // {
-  //   path: 'about-us',
-  //   loadChildren: () =>
-  //     import('./modules/about-us/about-us.module').then((m) => m.AboutUsModule),
-  // },
+  {path: '', component: HomeComponent},
+  {path: 'about-us', component: AboutUsComponent},
 ];
