37b7451111b5e6377dae23c057801c6ac8b289f1
Jeff Hwang <jeff@phntms.com>
2025-01-15 09:29:10 +1300

    Refactor routes

--- Projects/homes/src/main.ts
+++ Projects/homes/src/main.ts
@@ -1,9 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import routeConfig from './app/route';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routeConfig)],
+  providers: [provideRouter(routes)],
 }).catch((err) => console.error(err));
