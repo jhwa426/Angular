8bde2b45d8b7b4fbd40671ab990b1efb2a723020
Jeff Hwang <jeff@phntms.com>
2025-01-16 14:49:00 +1300

    Refactor void

--- Projects/E-Commerce-Store/client/src/app/services/cart.service.ts
+++ Projects/E-Commerce-Store/client/src/app/services/cart.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from '@angular/core';
-import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
-import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
-import { Cart, CartItem } from '../types/app.types';
+import { Injectable } from "@angular/core";
+import { MatLegacySnackBar as MatSnackBar } from "@angular/material/legacy-snack-bar";
+import { BehaviorSubject } from "rxjs/internal/BehaviorSubject";
+import { Cart, CartItem } from "../types/app.types";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class CartService {
   cart = new BehaviorSubject<Cart>({ items: [] });
 
   constructor(private _snackBar: MatSnackBar) {}
 
-  addToCart(item: CartItem): void {
+  addToCart(item: CartItem) {
     const items = [...this.cart.value.items];
 
     const itemInCart = items.find((_item) => _item.id === item.id);
@@ -22,17 +22,17 @@ export class CartService {
     }
 
     this.cart.next({ items });
-    this._snackBar.open('1 item added to cart.', 'Ok', { duration: 3000 });
+    this._snackBar.open("1 item added to cart.", "Ok", { duration: 3000 });
   }
 
   removeFromCart(item: CartItem, updateCart = true): CartItem[] {
     const filteredItems = this.cart.value.items.filter(
-      (_item) => _item.id !== item.id
+      (_item) => _item.id !== item.id,
     );
 
     if (updateCart) {
       this.cart.next({ items: filteredItems });
-      this._snackBar.open('1 item removed from cart.', 'Ok', {
+      this._snackBar.open("1 item removed from cart.", "Ok", {
         duration: 3000,
       });
     }
@@ -59,14 +59,14 @@ export class CartService {
     }
 
     this.cart.next({ items: filteredItems });
-    this._snackBar.open('1 item removed from cart.', 'Ok', {
+    this._snackBar.open("1 item removed from cart.", "Ok", {
       duration: 3000,
     });
   }
 
   clearCart(): void {
     this.cart.next({ items: [] });
-    this._snackBar.open('Cart is cleared.', 'Ok', {
+    this._snackBar.open("Cart is cleared.", "Ok", {
       duration: 3000,
     });
   }
